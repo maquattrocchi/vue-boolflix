150a83a7440a44398e32deb836ffa6f3cf52dfbc
maquattrocchi <martinaquattrocchi5@gmail.com>
2022-05-14 13:20:24 +0200

    Aggiunta dropdown-menu a NavComponent

--- src/components/NavComponent.vue
+++ src/components/NavComponent.vue
@@ -1,12 +1,19 @@
 <template>
+
     <div class="d-flex align-items-center gap-3">
         <h1>Boolflix</h1>
-        <ul>
-            <li><a href="#" >Home</a></li>
-            <li><a href="#">TV Show</a></li>
-            <li><a href="#">Movies</a></li>
-            <li><a href="#" class="selected">Recently Added</a></li>
-            <li><a href="#">My List</a></li>
+        <div class="dropdownd-block d-lg-none">
+            <div class="menu_title" @click="dropMenu = !dropMenu">Sfoglia <i class="fa-solid fa-angle-down"></i></div>
+            <ul class="cs_dropdown_menu" v-if="dropMenu">
+                <li v-for="(item, index) in menu" :key="index">
+                    <a :href="item.url" :class="{'selected' : item.selected}">{{item.link}}</a>
+                </li>
+            </ul>
+        </div>
+        <ul class="d-none d-lg-block">
+            <li v-for="(item, index) in menu" :key="index">
+                <a :href="item.url" :class="{'selected' : item.selected}">{{item.link}}</a>
+            </li>
         </ul>
     </div>
 </template>
@@ -14,6 +21,38 @@
 <script>
 export default {
     name:'NavComponent',
+    data(){
+        return {
+            menu: [
+                {
+                    link: 'Home',
+                    url: '#',
+                    selected: false,
+                },
+                {
+                    link: 'TV Show',
+                    url: '#',
+                    selected: false,
+                },
+                {
+                    link: 'Movies',
+                    url: '#',
+                    selected: false,
+                },
+                {
+                    link: 'Recently Added',
+                    url: '#',
+                    selected: true,
+                },
+                {
+                    link: 'My List',
+                    url: '#',
+                    selected: false,
+                },
+            ],
+            dropMenu: false
+        }
+    }
 }
 </script>
 
@@ -23,6 +62,29 @@ h1{
     text-transform: uppercase;
     color: $netflix-color;
     font-size: 3rem !important;
+    margin-bottom: 0 !important;
+}
+.menu_title{
+    font-size: 1.5rem;
+    cursor: pointer;
+}
+.cs_dropdown_menu{
+    position: absolute;
+    z-index: 1000;
+    min-width: 10rem;
+    padding: 0.5rem 0;
+    margin: 0;
+    font-size: 1rem;
+    text-align: left;
+    list-style: none;
+    background-clip: padding-box;
+    border: 1px solid rgba(0, 0, 0, 0.15);
+    border-radius: 0.25rem;
+    background-color: $bg-color;
+
+    & li{
+        display: block;
+    }
 }
 ul {
     padding: 0 !important;
--- src/components/SelectComponent.vue
+++ src/components/SelectComponent.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="text-center">
+    <div class="text-center mb-3">
         <select class="cs_select" name="genre" id="genre" @change="mySearch" v-model="selectOption">
             <option value="">Search by genre</option>
             <option :value="genre.id" v-for="(genre, index) in allGenres" :key="index">{{genre.name}}</option>
